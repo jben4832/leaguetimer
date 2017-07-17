5604b86eabe083ade56bddecf3c6efe3d9510a45
jben4832 <jben4832@uni.sydney.edu.au>
2017-07-17 17:18:08 +1000

    update

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+public function connect() {
+	$serverName = "leaguetimer.database.windows.net";
+	$connectionOptions = array(
+		"Database" => "leaguechamps",
+		"Uid" => "league",
+		"PWD" => "Assass69"
+	);
+	//Establishes the connection
+	$conn = sqlsrv_connect($serverName, $connectionOptions);
+	$tsql= "SELECT * FROM leaguechamps";
+	$getResults= sqlsrv_query($conn, $tsql);
+	if ($getResults == FALSE)
+		echo (sqlsrv_errors());
+	while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+	 echo ($row['ChampID'] . " " . $row['Name'] . PHP_EOL);
+	}
+	sqlsrv_free_stmt($getResults);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,7 +48,7 @@
                         <div class="col-sm-12">
                             <h1>League of Legends Spell Timers
 								  
-								<?php echo "hellow" ?>
+								connect();
 								
 								 
 							</h1>
