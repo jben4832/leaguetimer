e81a130a3b3db68f1d7362b4a3d194f31bb19904
jben4832 <jben4832@uni.sydney.edu.au>
2017-07-17 17:03:46 +1000

    update

--- champquery.php
+++ champquery.php
@@ -1,26 +1,21 @@
 <?php
-echo hi
 
-		$host = "leaguetimer.database.windows.net";
-		$user = "league";
-		$pwd = "Assass69";
-		$db = "LeagueChamps";
-		
-		try
-		{
-			$conn = new PDO("mysql:host=".$this->dbHostname.";dbname=".$this->dbDatabase, $this->dbUsername, $this->dbPassword);
-			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-		     $sql = "SELECT * FROM LeagueChamps;";
-			 $stmt =$conn->query($sql);
-			 $row = $stmt->fetch();
-		}
-		 catch(Exception $e)
-		{
-			 die(print_r($e));
-		}
 
-		echo "$row[0] $row[1]";
+$serverName = "leaguetimer.database.windows.net";
+$connectionOptions = array(
+    "Database" => "leaguechamps",
+    "Uid" => "league",
+    "PWD" => "Assass69"
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$tsql= "SELECT * FROM leaguechamps";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['ChampID'] . " " . $row['Name'] . PHP_EOL);
+}
+sqlsrv_free_stmt($getResults);
 
-		$conn = NULL;
 ?>
